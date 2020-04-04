d093c3687214e04a07e04ed107f859d1fe6da99d
Harys vizcaino <harys@mctekk.com>
2020-04-04 10:16:37 -0400

    created a example with merge

---
+++ src/combinacion/03-concat.ts
@@ -0,0 +1,10 @@
+import { interval, concat } from "rxjs";
+import { concatMap, take } from "rxjs/operators";
+
+
+const interval$ = interval(1000);
+
+concat(
+    interval$.pipe( take(3) ),
+    interval$.pipe( take(2) )
+).subscribe( console.log )
