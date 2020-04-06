dcc3f3ecaca75e42be5ec2512f6687922fbf9b5f
Harys vizcaino <harys@mctekk.com>
2020-04-06 10:46:19 -0400

    create countdown exercise

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,35 @@
-import { interval, Subject } from 'rxjs';
-import { take, map } from 'rxjs/operators';
+import { interval } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+
 /**
- * Ejercicio: Realizar que los dos observables finales, 
- * emitan exactamente el mismo valor
- * 
- * Tip: Hot Observable? subjects?
+ * Ejercicio: Realizar una cuenta regresiva
+ * empezando de 7
  */
+
+// Salida esperada ===
+// 7
+// 6
+// 5
+// 4
+// 3
+// 2
+// 1
+// 0
 
 (() =>{
 
-  // == NO TOCAR este bloque ====================
-  const reloj$ = interval(1000).pipe(
-    take(5),
-    map( val => Math.round(Math.random() * 100) )
-  );
-  // No tocar la creación del observable
-  // ============================================
+    const inicio = 7;
+    const countdown$ = interval(700).pipe(
+        // Usar los operadores necesarios
+        // para realizar la cuenta regresiva
+      map( i => inicio - i ),
+      take( inicio + 0 )
+    );
+    
 
-  // my subjectObs
-  
-  const obsSubject$ = new Subject();
-  
-  reloj$.subscribe(obsSubject$);
-  // Estos dos observables deben de emitir exactamente los mismos valores
-  obsSubject$.subscribe( val => console.log('obs1', val) );
-  obsSubject$.subscribe( val => console.log('obs2', val) );
-
-
-
+    // No tocar esta línea ==================
+    countdown$.subscribe( console.log ); // =
+    // ======================================
 
 
 })();
